f8e3c89366e73ad45158180f31e22ce1bdec3b07
erikmullagulov <erikmullagulov1@gmail.com>
2025-11-21 09:41:44 +0300

    Второй коммит (недоделанный)
    Добавлен метод для создания фреймов

--- src/main/kotlin/ru/gr05307/animation/FractalAnimation.kt
+++ src/main/kotlin/ru/gr05307/animation/FractalAnimation.kt
@@ -1,5 +1,40 @@
 package ru.gr05307.animation
 
-class FractalAnimation {
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.graphics.ImageBitmap
+import ru.gr05307.ui.PaintPanel
+import ru.gr05307.ui.SelectionPanel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+import ru.gr05307.painting.FractalPainter
+import ru.gr05307.viewmodels.MainViewModel
+import ru.gr05307.painting.convertation.Plain
+
+class FractalAnimation(
+    private val firstFramePlain: Plain,
+    private val lastFramePlain: Plain,
+    private val viewModel: MainViewModel,
+    private val fractalPainter: FractalPainter,
+    private val animationOn: Boolean
+) {
+    private val fractalFrames = mutableListOf<ImageBitmap>()
+    private val width = viewModel.fractalImage.width
+    private val height = viewModel.fractalImage.height
+
+    private fun createFrames(nFrames: Int = 30) {
+        for (n in 1..nFrames){
+            val d = n.toDouble() / nFrames
+            val tempFramePlain = Plain(
+                xMin = (lastFramePlain.xMin - firstFramePlain.xMin) * d + firstFramePlain.xMin,
+                xMax = (lastFramePlain.xMax - firstFramePlain.xMax) * d + firstFramePlain.xMax,
+                yMin = (lastFramePlain.yMin - firstFramePlain.yMin) * d + firstFramePlain.yMin,
+                yMax = (lastFramePlain.yMax - firstFramePlain.yMax) * d + firstFramePlain.yMax
+            )
+        }
+    }
+
+    private suspend fun startAnimation() = withContext(Dispatchers.Default) {
+    }
 
 }
